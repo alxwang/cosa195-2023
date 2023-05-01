65c8886fe0ab18a1839d924896d3214cdad0d904
Alex Wang <wangxi@saskpolytech.ca>
2023-05-01 14:38:17 -0600

    start template

--- cosa195-template/src/main/java/Server.java
+++ cosa195-template/src/main/java/Server.java
@@ -1,8 +1,14 @@
 import ServiceController.AirCraftServiceController;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class Server {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create(
+                config->{
+                    config.staticFiles.add("/htdocs", Location.CLASSPATH);
+                }
+                )
                 .get("/",
                         ctx -> ctx.result("Welcome to COSA 195 App Server."))
                 .start(7070);
